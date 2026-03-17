6126e5a47edc58c1704f60947cf2397c00a70028
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2026-03-17 20:38:16 +0100

    Added DMI.SMS.Domain

---
+++ DMI.SMS/DMI.SMS.Domain/Entities/ServiceVisitReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DMI.SMS.Domain.Entities
+{
+    public class ServiceVisitReport : ChildEntity
+    {
+        private StationInformation _stationinformation;
+        private DateTime? _date;
+        private string? _serviceId;
+
+        public StationInformation StationInformation
+        {
+            get { return _stationinformation; }
+            set { _stationinformation = value; }
+        }
+
+        public DateTime? Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public string? ServiceId
+        {
+            get { return _serviceId; }
+            set { _serviceId = value; }
+        }
+    }
+}
