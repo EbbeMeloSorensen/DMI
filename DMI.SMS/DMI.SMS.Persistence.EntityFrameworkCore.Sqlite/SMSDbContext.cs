3029259ee2ef10bcf762334357bda188eefb4d85
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2026-03-19 19:44:13 +0100

    -

---
+++ DMI.SMS/DMI.SMS.Persistence.EntityFrameworkCore.Sqlite/SMSDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DMI.SMS.Domain.Entities;
+using DMI.SMS.Persistence.EntityFrameworkCore.EntityConfigurations;
+
+namespace DMI.SMS.Persistence.EntityFrameworkCore.Sqlite
+{
+    public class SMSDbContext : SMSDbContextBase
+    {
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = ConnectionStringProvider.GetConnectionString();
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
---
+++ DMI.SMS/DMI.SMS.Persistence.EntityFrameworkCore.Sqlite/UnitOfWorkFactory.cs
@@ -0,0 +1,36 @@
+﻿using Craft.Logging;
+
+namespace DMI.SMS.Persistence.EntityFrameworkCore.Sqlite
+{
+    public class UnitOfWorkFactory : UnitOfWorkFactoryBase
+    {
+        static UnitOfWorkFactory()
+        {
+            using var context = new SMSDbContext();
+            context.Database.EnsureCreated();
+
+            if (context.StationInformations.Any() ||
+                context.SensorLocations.Any()) return;
+
+            Seeding.SeedDatabase(context);
+        }
+
+        public override void Initialize(ILogger logger)
+        {
+        }
+
+        public override async Task<bool> CheckRepositoryConnection()
+        {
+            return await Task.Run(() =>
+            {
+                using var context = new SMSDbContext();
+                return context.Database.CanConnect();
+            });
+        }
+
+        public override IUnitOfWork GenerateUnitOfWork()
+        {
+            return new UnitOfWork(new SMSDbContext());
+        }
+    }
+}
