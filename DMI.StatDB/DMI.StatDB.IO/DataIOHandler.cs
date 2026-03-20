7405802f5c67250230af8bb6dde562961f31fc7b
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2026-03-20 18:14:52 +0100

    -

---
+++ DMI.StatDB/DMI.StatDB.IO/DataIOHandler.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DMI.StatDB.Domain.Entities;
+using Newtonsoft.Json;
+using Craft.IO.Utils;
+
+namespace DMI.StatDB.IO
+{
+    public class DataIOHandler : IDataIOHandler
+    {
+        public void ExportDataToXML(
+            IList<Station> stationInformations,
+            IList<Position> positions,
+            string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExportDataToJson(
+            IList<Station> stations, 
+            IList<Position> positions, 
+            string fileName)
+        {
+            var jsonResolver = new ContractResolver();
+            jsonResolver.IgnoreProperty(typeof(Station), "Positions");
+            jsonResolver.IgnoreProperty(typeof(Position), "Station");
+
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = jsonResolver
+            };
+
+            var statDBData = new StatDBData
+            {
+                Stations = stations.ToList(),
+                Positions = positions.ToList()
+            };
+
+            var json = JsonConvert.SerializeObject(
+                statDBData, 
+                Formatting.Indented, 
+                settings);
+
+            using (var streamWriter = new StreamWriter(fileName))
+            {
+                streamWriter.WriteLine(json);
+            }
+        }
+
+        public void ImportDataFromXML(
+            string fileName, 
+            out IList<Station> stationInformations,
+            out IList<Position> positions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ImportDataFromJson(
+            string fileName, 
+            out IList<Station> stations,
+            out IList<Position> positions)
+        {
+            using (var streamReader = new StreamReader(fileName))
+            {
+                var json = streamReader.ReadToEnd();
+                var statDBData = JsonConvert.DeserializeObject<StatDBData>(json);
+                stations = statDBData.Stations;
+                positions = statDBData.Positions;
+            }
+
+            var nextId = 1;
+        }
+    }
+}
---
+++ DMI.StatDB/DMI.StatDB.IO/IDataIOHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using DMI.StatDB.Domain.Entities;
+
+namespace DMI.StatDB.IO
+{
+    public interface IDataIOHandler
+    {
+        void ExportDataToXML(
+            IList<Station> stations,
+            IList<Position> positions,
+            string fileName);
+
+        void ExportDataToJson(
+            IList<Station> stations,
+            IList<Position> positions,
+            string fileName);
+
+        void ImportDataFromXML(
+            string fileName,
+            out IList<Station> stations,
+            out IList<Position> positions);
+
+        void ImportDataFromJson(
+            string fileName,
+            out IList<Station> stations,
+            out IList<Position> positions);
+    }
+}
