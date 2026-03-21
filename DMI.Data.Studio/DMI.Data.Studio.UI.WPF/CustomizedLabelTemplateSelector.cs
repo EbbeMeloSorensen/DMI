b23cef36e211cbf641d852be55a3e6d6742cb0c4
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2026-03-21 12:21:37 +0100

    -

---
+++ DMI.Data.Studio/DMI.Data.Studio.UI.WPF/CustomizedLabelTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using Craft.ViewModels.Geometry2D.ScrollFree;
+using DMI.Data.Studio.ViewModel;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DMI.Data.Studio.UI.WPF;
+
+public class CustomizedLabelTemplateSelector : DataTemplateSelector
+{
+    public override DataTemplate SelectTemplate(
+        object item,
+        DependencyObject container)
+    {
+        var element = container as FrameworkElement;
+
+        //return element.FindResource("TransactionTimeIntervalLabel") as DataTemplate;
+
+        return item switch
+        {
+            PositionDifferenceLabel => element.FindResource("PositionDifferenceLabel") as DataTemplate,
+            TransactionTimeIntervalLabel => element.FindResource("TransactionTimeIntervalLabel") as DataTemplate,
+            LabelViewModel => element.FindResource("Label") as DataTemplate,
+            _ => throw new ArgumentException("item doesn't correspond to any DataTemplate")
+        };
+
+    }
+}
---
+++ DMI.Data.Studio/DMI.Data.Studio.UI.WPF/CustomizedShapeTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using DMI.Data.Studio.ViewModel;
+
+namespace DMI.Data.Studio.UI.WPF
+{
+    public class CustomizedShapeTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(
+            object item,
+            DependencyObject container)
+        {
+            var element = container as FrameworkElement;
+
+            return item switch
+            {
+                LightGreenBar => element.FindResource("LightGreenBar") as DataTemplate,
+                LightRedBar => element.FindResource("LightRedBar") as DataTemplate,
+                GrayBar => element.FindResource("GrayBar") as DataTemplate,
+                RedBar => element.FindResource("RedBar") as DataTemplate,
+                OrangeBar => element.FindResource("OrangeBar") as DataTemplate,
+                GreenBar => element.FindResource("GreenBar") as DataTemplate,
+                YellowBar => element.FindResource("YellowBar") as DataTemplate,
+                _ => throw new ArgumentException("item doesn't correspond to any DataTemplate")
+            };
+        }
+    }
+}
