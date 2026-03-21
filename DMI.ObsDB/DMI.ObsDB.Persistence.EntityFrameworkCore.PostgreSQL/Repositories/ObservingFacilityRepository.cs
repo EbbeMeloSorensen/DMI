0b09a0088fa52a522eae44b39206173fce29fc30
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2026-03-21 09:47:36 +0100

    -

---
+++ DMI.ObsDB/DMI.ObsDB.Persistence.EntityFrameworkCore.PostgreSQL/Repositories/ObservingFacilityRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Craft.Persistence.EntityFrameworkCore;
+using DMI.ObsDB.Domain.Entities;
+using DMI.ObsDB.Persistence.Repositories;
+
+namespace DMI.ObsDB.Persistence.EntityFrameworkCore.PostgreSQL.Repositories
+{
+    public class ObservingFacilityRepository : Repository<ObservingFacility>, IObservingFacilityRepository
+    {
+        public ObservingFacilityRepository(DbContext context) : base(context)
+        {
+        }
+
+        public ObservingFacility Get(
+            int id)
+        {
+            var context = Context as ObsDBContext;
+
+            return context.ObservingFacilities
+                .SingleOrDefault(_ => _.Id == id) ?? throw new InvalidOperationException();
+        }
+
+        public ObservingFacility GetIncludingTimeSeries(
+            int id)
+        {
+            var context = Context as ObsDBContext;
+
+            return context.ObservingFacilities
+                .Include(_ => _.TimeSeries)
+                .SingleOrDefault(_ => _.Id == id) ?? throw new InvalidOperationException();
+        }
+
+        public override async Task Clear()
+        {
+            await Task.Run(() =>
+            {
+                var context = Context as ObsDBContext;
+
+                context.RemoveRange(context.ObservingFacilities);
+            });
+        }
+
+        public override Task Update(ObservingFacility entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task UpdateRange(IEnumerable<ObservingFacility> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
